369b51e9553954f521bb1b635e2934bd75969ea5
happyg920624 <51997834+happyg920624@users.noreply.github.com>
2019-06-27 15:06:18 +0800

    Add files via upload

---
+++ Example20190605.ino
@@ -0,0 +1,26 @@
+
+
+void setup()
+{
+   pinMode(6,OUTPUT); 
+   pinMode(9,OUTPUT);
+   pinMode(13,OUTPUT);
+}
+
+void loop()
+{
+    digitalWrite(6, HIGH);
+    delay(1000);
+    digitalWrite(6, LOW);
+    delay(1000);
+    digitalWrite(9, HIGH);
+    delay(10);
+    digitalWrite(9, LOW);
+    delay(10);
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(100);
+}
+
+
